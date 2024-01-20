29cb44adcfd0955ffe019fe308db4607b3db8f70
Shekaju007 <shekharsanju97@gmail.com>
2024-01-19 20:15:54 -0600

    njj

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { DatafetchService } from './datafetch.service';
 
 @Component({
   selector: 'app-root',
