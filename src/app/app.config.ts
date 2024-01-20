3d7e944428e586a9bf005cbb036824116d280457
Shekaju007 <shekharsanju97@gmail.com>
2024-01-19 20:01:52 -0600

    UICODE

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,58 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
+import { AdminRegisterComponent } from './admin-register/admin-register.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule ,NoopAnimationsModule} from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSelectModule } from '@angular/material/select';
+import { MatOptionModule } from '@angular/material/core';
+import { DatafetchService } from './datafetch.service';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { HomePageComponent } from './home-page/home-page.component';
+import { TokenInterceptor } from './interceptor/token.interceptor';
+import { AppRoutingModule } from './app.routes';
 
-import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
 
-export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
-};
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AdminLoginComponent,
+    AdminRegisterComponent,
+    HomePageComponent
+  ],
+  imports: [
+  BrowserAnimationsModule,
+  NoopAnimationsModule,
+  BrowserModule,
+  AppRoutingModule,
+  FlexLayoutModule,
+  FormsModule,
+  ReactiveFormsModule,
+  MatToolbarModule,
+  MatInputModule,
+  MatCardModule,
+  MatMenuModule,
+  MatIconModule,
+  MatButtonModule,
+  MatTableModule,
+  MatSlideToggleModule,
+  MatSelectModule,
+  MatOptionModule,
+  HttpClientModule
+  
+  ],
+  providers: [{provide:HTTP_INTERCEPTORS,useClass:TokenInterceptor,multi:true}],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,32 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
+import { AdminRegisterComponent } from './admin-register/admin-register.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { AuthGuard } from './guard/auth.guard';
 
-export const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: AdminLoginComponent
+  },
+
+  {
+    path: 'register',
+    component: AdminRegisterComponent
+  },
+  {
+    path: 'home',
+    component: HomePageComponent,
+    canActivate:[AuthGuard]
+  },
+];
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+
+ }
